6ebdecfa7c447b77e7220731a736611e0f52feb7
sanjaysanthosh140 <sanjaysanthosh140@gmail.com>
2025-10-15 10:34:12 +0530

    authentication debugger

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ const ApolloServer_start = async () => {
                         });
 
                         if (user_data.ok) {
+                            console.log("this is a debugger authentication response ",user_data)
                             const user = await user_data.json();
                             console.log("Auth result from Server 1:", user);
                             return { user: user };
